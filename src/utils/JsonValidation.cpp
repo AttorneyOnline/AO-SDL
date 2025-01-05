824d12a494aef7d53626c63dc99f715edb74cd12
scatterflower <marisaposs@proton.me>
2025-01-05 15:13:17 -0800

    fix exception type thrown by json validator

--- src/utils/JsonValidation.cpp
+++ src/utils/JsonValidation.cpp
@@ -1,16 +1,17 @@
 #include "JsonValidation.h"
 
 #include <format>
+#include <stdexcept>
 
 void JsonValidation::containsKeys(const nlohmann::json& json_data, const ValidationSettings& settings) {
     for (const auto& [key, value_type] : settings) {
 
         if (!json_data.contains(key)) {
-            throw std::exception(std::format("Missing key {} in json object", key).c_str());
+            throw std::runtime_error(std::format("Missing key {} in json object", key).c_str());
         }
 
         if (json_data[key].type() != value_type) {
-            throw std::exception(std::format("Type mismatch on key {}", key).c_str());
+            throw std::runtime_error(std::format("Type mismatch on key {}", key).c_str());
         }
     }
 }
