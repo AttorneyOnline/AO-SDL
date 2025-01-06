7a0f35086e522acc50ba6b022fc36d3927fa110c
Salanto <62221668+Salanto@users.noreply.github.com>
2025-01-06 20:14:58 +0100

    Interface for mounts

--- src/asset/MountAccess.h
+++ src/asset/MountAccess.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <filesystem>
 #include <string>
 #include <vector>
 
+enum MountType { Filesystem, Archive };
+
 class MountAccess {
   public:
-    enum MountType { Filesystem, Archive };
+    MountAccess(std::filesystem::path location) : mount_path{location} {};
 
-    virtual std::vector<std::byte> fetch_data(std::string relative_path) = 0;
+    using MediaData = std::optional<std::vector<std::byte>>;
+
+    virtual MediaData fetch_data(std::string relative_path) = 0;
+    virtual std::filesystem::path path() = 0;
+    virtual MountType type() = 0;
+
+  private:
+    virtual bool loadCache() = 0;
+    virtual bool saveCache() = 0;
+
+    std::filesystem::path mount_path;
 };
